b1f1ec9784df26d0c80e769022ed503daa3fe3af
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-02-09 15:30:59 +0200

    comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(buildGreeting("World"));
     }
-
+//comment
     public static String buildGreeting(String name) {
         return "Hello " + name + "!";
     }
